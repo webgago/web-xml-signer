5a19a9b51fead4b0d55db3ffb8fac65229c76e5e
Michael Bashkirov <bashmish@gmail.com>
2013-04-11 21:11:59 +0400

    Preparation is unnecessary and moreover wrong.

--- Gemfile
+++ Gemfile
@@ -2,7 +2,6 @@ source 'http://rubygems.org/'
 
 gem 'sinatra'
 gem 'thin'
-gem 'nokogiri'
 gem 'xmldsign'
 
 group :development do
--- app.rb
+++ app.rb
@@ -5,14 +5,6 @@ require 'sinatra'
 
 Bundler.require(:default, settings.environment)
 
-def prepare_xml(xml)
-  doc = Nokogiri.XML(xml) do |config|
-    config.default_xml.noblanks
-  end
-  root_node = doc.root
-  root_node.to_xml(save_with: Nokogiri::XML::Node::SaveOptions::AS_XML, encoding: 'UTF-8')
-end
-
 set :server, %w[thin webrick]
 
 get '/' do
@@ -20,14 +12,14 @@ get '/' do
 end
 
 post '/prepare_signed_info' do
-  xmldsign_document = Xmldsign::Document.string(prepare_xml(params[:xml]))
+  xmldsign_document = Xmldsign::Document.string(params[:xml])
   signature = xmldsign_document.signature
   signature.fill_digest!
   signature.c14n_signed_info
 end
 
 post '/insert_signature_and_certificate' do
-  xmldsign_document = Xmldsign::Document.string(prepare_xml(params[:xml]))
+  xmldsign_document = Xmldsign::Document.string(params[:xml])
   xmldsign_document.signature.fill_digest!
   signature_value_node = xmldsign_document.find_first('.//ds:SignatureValue')
   signature_value_node.content = params[:signature_value]
